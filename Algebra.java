e5f31d4cd9d23ec947ad58e11646a250b7f79524
Daniel Malede <danielmalede@gmail.com>
2024-11-26 19:07:14 +0200

    plus  minus times pow

--- Algebra.java
+++ Algebra.java
@@ -8,13 +8,13 @@ import java.lang.reflect.Array;
 public class Algebra {
 	public static void main(String args[]) {
 		// Tests some of the operations
-		System.out.println(plus(-5, 1)); // 2 + 3
-		// System.out.println(minus(7, 2)); // 7 - 2
-		// System.out.println(minus(2, 7)); // 2 - 7
-		// System.out.println(times(3, 4)); // 3 * 4
-		// System.out.println(plus(2, times(4, 2))); // 2 + 4 * 2
-		// System.out.println(pow(5, 3)); // 5^3
-		// System.out.println(pow(3, 5)); // 3^5
+		System.out.println(plus(0, 0)); // 2 + 3
+		System.out.println(minus(7, 2)); // 7 - 2
+		System.out.println(minus(-5, -5)); // 2 - 7
+		System.out.println(times(0, 0)); // 3 * 4
+		System.out.println(plus(2, times(4, 2))); // 2 + 4 * 2
+		System.out.println(pow(2, 10)); // 5^3
+		System.out.println(pow(3, 5)); // 3^5
 		// System.out.println(div(12, 3)); // 12 / 3
 		// System.out.println(div(5, 5)); // 5 / 5
 		// System.out.println(div(25, 7)); // 25 / 7
@@ -27,8 +27,25 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
 		int sum = 0;
+		if (x1 > x2) {
+			sum = x2;
+			for (int i = 0; i < x1; i++) {
+				sum += 1;
+			}
+		}
+		if (x2 > x1) {
+			sum = x1;
+			for (int i = 0; i < x2; i++) {
+				sum += 1;
+			}
+		}
+		if (x1 == x2) {
+			sum = x1;
+			for (int i = 0; i < x2; i++) {
+				sum += 1;
+			}
+		}
 		if (x1 < 0 && x2 < 0) {
 			if (x1 < x2) {
 				sum = x2;
@@ -49,45 +66,90 @@ public class Algebra {
 				}
 			}
 		}
+
+		return sum;
+	}
+
+	// Returns x1 - x2
+	public static int minus(int x1, int x2) {
+		int sum = 0;
 		if (x1 > x2) {
-			sum = x2;
-			for (int i = 0; i < x1; i++) {
-				sum += 1;
+			sum = x1;
+			for (int i = 0; i < x2; i++) {
+				sum -= 1;
 			}
 		}
 		if (x2 > x1) {
 			sum = x1;
 			for (int i = 0; i < x2; i++) {
-				sum += 1;
+				sum -= 1;
 			}
 		}
 		if (x1 == x2) {
 			sum = x1;
 			for (int i = 0; i < x2; i++) {
+				sum -= 1;
+			}
+		}
+		if (x1 < 0 && x2 < 0) {
+			sum = x1;
+			for (int i = 0; i > x2; i--) {
 				sum += 1;
+			}
+			if (x1 == x2) {
+				sum = x1;
+				for (int i = 0; i > x2; i--) {
+					sum -= -1;
+				}
 			}
 		}
 
 		return sum;
 	}
 
-	// // Returns x1 - x2
-	// public static int minus(int x1, int x2) {
-	// // Replace the following statement with your code
-	// return 0;
-	// }
-
 	// // Returns x1 * x2
-	// public static int times(int x1, int x2) {
-	// // Replace the following statement with your code
-	// return 0;
-	// }
+	public static int times(int x1, int x2) {
+		int sum = 0;
+		if (x1 < 0 || x2 < 0) {
+			if (x2 < 0) {
+				for (int i = 0; i > x2; i--) {
+					sum += x1;
+				}
+				sum = -sum;
+			} else {
+				for (int i = 0; i < x2; i++) {
+					sum += x1;
+				}
+			}
+		}
+		if (x1 < 0 && x2 < 0) {
+			sum = 0;
+			for (int i = 0; i > x1; i--) {
+				sum += x2;
+			}
+			sum = -sum;
+		}
+		if (x1 > 0 && x2 > 0) {
+			sum = 0;
+			for (int i = 0; i < x1; i++) {
+				sum += x2;
+			}
+		}
+		if (x1 == 0 || x2 == 0) {
+			sum = 0;
+		}
+
+		return sum;
+	}
 
 	// // Returns x^n (for n >= 0)
-	// public static int pow(int x, int n) {
-	// // Replace the following statement with your code
-	// return 0;
-	// }
+	public static int pow(int x, int n) {
+		int temp = 1;
+		for (int i = 0; i < n; i++) {
+			temp = times(temp, x);
+		}
+		return temp;
+	}
 
 	// // Returns the integer part of x1 / x2
 	// public static int div(int x1, int x2) {
